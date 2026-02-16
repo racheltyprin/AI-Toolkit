9510919fe8807c7933db3bf0bda4e4158b0dcaf5
racheltyprin <racheltyprindrexel@gmail.com>
2026-02-16 13:09:07 -0800

    restart

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var main = document.querySelector('.main-content');
-    if (main) main.scrollTop = 0;
-    window.scrollTo(0, 0);
-});
-
